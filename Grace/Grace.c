9e5d5bbf7062eca06a16866e33fa406aa9828e29
Andrew VALLETEAU <avallete@e1r7p15.42.fr>
2016-12-19 18:53:15 +0100

    [WIP] Grace challenge

---
+++ Grace/Grace.c
@@ -0,0 +1,8 @@
+/*
+  Grace comment
+*/
+#include<stdio.h>
+#define FT1 FT2
+#define FT2 int
+#define FT(x) FT1 main(){ FILE* f; char *fmt = "%c*%c  Grace comment%c*%c%c#include<stdio.h>%c#define FT1 FT2%c#define FT2 int%c#define FT(x) FT1 main(){ FILE* f; char *fmt = %c%s%c; if ((f = fopen(%cGrace_kid.c%c, %cw+%c))){fprintf(f,fmt,47,10,10,47,10,10,10,10,34,fmt,34,34,34,34,34,fmt,10);}}%cFT(x)"; if ((f = fopen("Grace_kid.c", "w+"))){fprintf(f,fmt,47,10,10,47,10,10,10,10,34,fmt,34,34,34,34,34,fmt,10);}}
+FT(xxxxxx)
