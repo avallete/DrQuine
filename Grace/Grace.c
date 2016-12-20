6300d17a6ebaf38494fff07a03151ff2ae7b4527
Andrew Valleteau <avallete@student.42.fr>
2016-12-20 21:49:44 +0100

    [Feat] Grace done.

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -4,5 +4,5 @@
 #include<stdio.h>
 #define FT1 FT2
 #define FT2 int
-#define FT(x) FT1 main(){ FILE* f; char *fmt = "%c*%c  Grace comment%c*%c%c#include<stdio.h>%c#define FT1 FT2%c#define FT2 int%c#define FT(x) FT1 main(){ FILE* f; char *fmt = %c%s%c; if ((f = fopen(%cGrace_kid.c%c, %cw+%c))){fprintf(f,fmt,47,10,10,47,10,10,10,10,34,fmt,34,34,34,34,34,fmt,10);}}%cFT(x)"; if ((f = fopen("Grace_kid.c", "w+"))){fprintf(f,fmt,47,10,10,47,10,10,10,10,34,fmt,34,34,34,34,34,fmt,10);}}
-FT(xxxxxx)
+#define FT(x) FT2 main(){ FILE* f; char *fmt = "%c*%c  Grace comment%c*%c%c#include<stdio.h>%c#define FT1 FT2%c#define FT2 int%c#define FT(x) FT2 main(){ FILE* f; char *fmt = %c%s%c; if ((f = fopen(%cGrace_kid.c%c, %cw+%c))){fprintf(f,fmt,47,10,10,47,10,10,10,10,34,fmt,34,34,34,34,34,10,#x);}}%cFT(%s)"; if ((f = fopen("Grace_kid.c", "w+"))){fprintf(f,fmt,47,10,10,47,10,10,10,10,34,fmt,34,34,34,34,34,10,#x);}}
+FT(xxxxxxxx)
