99f2749638bee06888801f4310fe8b63598ab87d
Andrew Valleteau <avallete@student.42.fr>
2016-12-22 00:22:05 +0100

    [WIP] Sully almost done

--- Sully/Sully.c
+++ Sully/Sully.c
@@ -1,9 +1,10 @@
+#define _GNU_SOURCE
 #include<stdlib.h>
 #include<stdio.h>
 int main(){
-    int i = 5;
-    char *name;
-    char *cmd;
-    FILE *f;
-    char *fmt="#include<stdlib.h>%c#include<stdio.h>%cint main(){%c    int i = %d;%c    char *name;%c    char *cmd;%c    FILE *f;%cchar *fmt=%c%s%c;%c    if (asprintf(&name, %cSully_%%d.c%c, i) >= 0 && asprintf(&cmd, %cgcc %%s; ./a.out%c, name) >= 0){if (i >= 0 && (f = fopen(name, %cw+%c))){fprintf(f, fmt,10,10,10,i-1,10,10,10,10,34,fmt,34,10,34,34,34,34,34,34);system(cmd);}}}";
-    if (asprintf(&name, "Sully_%d.c", i) >= 0 && asprintf(&cmd, "gcc %s; ./a.out", name) >= 0){if (i >= 0 && (f = fopen(name, "w+"))){fprintf(f, fmt,10,10,10,i-1,10,10,10,10,34,fmt,34,10,34,34,34,34,34,34);system(cmd);}}}
+int i = 5;
+char *name;
+char *cmd;
+FILE *f;
+char *fmt="#define _GNU_SOURCE%c#include<stdlib.h>%c#include<stdio.h>%cint main(){%cint i = %d;%cchar *name;%cchar *cmd;%cFILE *f;%cchar *fmt=%c%s%c;%cif (asprintf(&name, %cSully_%%d.c%c, i-1) >= 0 && asprintf(&cmd, %cgcc %%s -o %%s.exe; ./%%s.exe%c, name, name, name) >= 0){if (i > 0 && (f = fopen(name, %cw+%c))){fprintf(f, fmt,10,10,10,10,i-1,10,10,10,10,34,fmt,34,10,34,34,34,34,34,34);fclose(f);system(cmd);}}return(0);}";
+if (asprintf(&name, "Sully_%d.c", i-1) >= 0 && asprintf(&cmd, "gcc %s -o %s.exe; ./%s.exe", name, name, name) >= 0){if (i > 0 && (f = fopen(name, "w+"))){fprintf(f, fmt,10,10,10,10,i-1,10,10,10,10,34,fmt,34,10,34,34,34,34,34,34);fclose(f);system(cmd);}}return(0);}
